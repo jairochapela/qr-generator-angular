431d1c3f922383a9f4f10f8c0252b1e041558adb
Jairo Chapela-Martínez <jairochapela@gmail.com>
2018-04-21 17:19:38 +0200

    Reactive form.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My QR Generator';
+  mode = 'url';
+  formData = new FormGroup({
+    mode: new FormControl(),
+    url: new FormControl()
+  });
 }
