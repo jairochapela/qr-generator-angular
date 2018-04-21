3dffdc56e916e8a4c165e77e611507c3985f5b66
Jairo Chapela-Martínez <jairochapela@gmail.com>
2018-04-21 19:08:57 +0200

    Initial state.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,21 @@ export class AppComponent {
   title = 'My QR Generator';
   mode = 'url';
   formData = new FormGroup({
-    mode: new FormControl(),
-    url: new FormControl(),
-    text: new FormControl()
+    mode: new FormControl({value: 'url'}),
+    url: new FormControl({value:'', disabled:false}),
+    text: new FormControl({value:'', disabled:false})
   });
+
+  onChangeMode() {
+    console.log("clunk " + this.currentMode())
+    let mode = this.currentMode();
+    // if (mode==='url') this.formData.get('url').enable(); else this.formData.get('url').disable();
+    // if (mode==='text') this.formData.get('text').enable(); else this.formData.get('text').disable();
+  }
+
+  currentMode() : string {
+    return this.formData.get('mode').value;
+  }
 
   getDataForQR() : string {
     if (this.formData.get('mode').value === 'url') return this.formData.get('url').value;
